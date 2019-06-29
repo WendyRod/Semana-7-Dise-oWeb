6c5567dd4aa4565e7738fae29d5efa996aadfe10
WendyRod <wendypame711@gmail.com>
2019-06-28 20:32:40 -0600

    1 file modified

--- index.js
+++ index.js
@@ -3,6 +3,49 @@ import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
 
+class Contador extends Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      contador :1
+    }
+  }
+  render (){
+    return (
+      <div>
+      <p>Contador : {this.state.contador}</p>
+      <button onClick={()=>{
+        this.setState({
+          contador: this.state.contador +1
+        })}}>Aumentar</button>
+      <button onClick={()=>{
+        if(this.state.contador==0){
+
+        }else{
+        this.setState({
+          contador: this.state.contador -1
+        })}}}>Disminuir</button>
+      </div>
+      
+    )
+  }
+}
+
+//let nombre = "Miguel Castro"
+function A(props) {
+  return <p> un dios {props.nombre} </p>
+}
+
+function B(props) {
+  return <p> un dios {props.nombre} </p>
+}
+
+class MicomponenteClase extends Component {
+  render() {
+    return <p> un dios 2 </p>
+  }
+}
+
 class App extends Component {
   constructor() {
     super();
@@ -14,10 +57,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
+      <Contador/>
       </div>
     );
   }
